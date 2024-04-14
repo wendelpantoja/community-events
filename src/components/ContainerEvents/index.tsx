faaf91dc76459639efd7a562940d20d44b5abc0b
Jose Wendel <wendelpviana@gmail.com>
2024-04-14 16:51:01 -0300

    :sparkles: feat: finish slide swiper

--- src/components/ContainerEvents/index.tsx
+++ src/components/ContainerEvents/index.tsx
@@ -2,14 +2,16 @@ import { Container } from "../../styles/GlobalStyles";
 import { Card } from "../Card";
 
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
 
-import { ContainerEventsComponent } from "./styles";
+import { ContainerEventsComponent, PlusEvents } from "./styles";
+import { useEvent } from "../../context/EventProvider/useEvent";
+import { Navigation } from "swiper/modules";
+import { useState } from "react";
 
 import 'swiper/css';
 import 'swiper/css/navigation';
-import { useEvent } from "../../context/EventProvider/useEvent";
-
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 interface CardContainerProps {
     event?: string;
@@ -18,53 +20,80 @@ interface CardContainerProps {
 export function ContainerEvents({event}: CardContainerProps) {
 
     const { events } = useEvent()
+    const [indexSwiper, setIndexSwiper] = useState<number>(0)
+    const [isEnd, setIsEnd] = useState<boolean>(false)
     
     return (
         <Container>
             <ContainerEventsComponent>
                 <h1 className="">Evento {event}</h1>
                 <Swiper 
-                     slidesPerView={1.3}
-                     spaceBetween={15}
-                     breakpoints={{
-                        450: {
-                            slidesPerView: 1.5,
-                            spaceBetween: 10,
+                    className="container_swiper"
+                    modules={[Navigation]}
+                    slidesPerView={1.3}
+                    spaceBetween={15}
+                    breakpoints={{
+                    450: {
+                        slidesPerView: 1.5,
+                        spaceBetween: 10,
+                    },
+                    640: {
+                        slidesPerView: 2,
+                        spaceBetween: 5,
+                    },
+                    768: {
+                        slidesPerView: 2,
+                        spaceBetween: 5,
+                    },
+                    970: {
+                        slidesPerView: 3.3,
+                        spaceBetween: 5,
                         },
-                        640: {
-                            slidesPerView: 2.3,
-                            spaceBetween: 20,
-                        },
-                        768: {
-                            slidesPerView: 2.5,
-                            spaceBetween: 20,
-                        },
-                        970: {
-                            slidesPerView: 3.3,
-                            spaceBetween: 20,
-                            },
-                        1024: {
-                            slidesPerView: 3.5,
-                            spaceBetween: 20,
-                        },
-                        1085: {
-                            slidesPerView: 4,
-                            spaceBetween: 20,
-                        },
-                        1350: {
-                        slidesPerView: 4.5,
-                        spaceBetween: 20,
-                        },
-                     }}
+                    1024: {
+                        slidesPerView: 3.5,
+                        spaceBetween: 5,
+                    },
+                    1085: {
+                        slidesPerView: 4,
+                        spaceBetween: 5,
+                    },
+                    1350: {
+                    slidesPerView: 4.5,
+                    spaceBetween: 20,
+                    },
+                    }}
+                    navigation={{
+                        nextEl: ".button_next",
+                        prevEl: ".button_prev"
+                    }}
+                    onSlideChange={(swiper) => {
+                        setIsEnd(swiper.isEnd)
+                        setIndexSwiper(swiper.activeIndex)
+                    }}
                 >
                     {events?.filter(type => type.data().tipo_evento.toLowerCase() === event?.toLowerCase())
-                    .map((event) => (
-                        <SwiperSlide key={event.id}>
-                            <Card 
-                                data={event}
-                            />
+                    .slice(0, 9)
+                    .map((event, index) => (
+                        <SwiperSlide className="swiper_slide" key={event.id}>
+                            {index === 8 ? (
+                                <PlusEvents>
+                                    <p>Ver mais</p>
+                                </PlusEvents>
+                            ) : (
+                                <Card 
+                                    data={event}
+                                />
+                            )}
                         </SwiperSlide>
                     ))}
+                    
+                    <button className={`button_prev ${indexSwiper === 0 ? "desable" : ""}`}>
+                        <i className='bx bxs-chevron-left swiper-button-disabled'></i>
+                    </button>
+    
+                    <button className={`button_next ${isEnd === true ? "desable" : ""}`}>
+                        <i className='bx bxs-chevron-right'></i>
+                    </button>
                 </Swiper>
             </ContainerEventsComponent>
         </Container>
--- src/components/ContainerEvents/styles.ts
+++ src/components/ContainerEvents/styles.ts
@@ -8,11 +8,85 @@ export const ContainerEventsComponent = styled.section`
         margin-bottom: 25px;
     }
 
+    .container_swiper {
+        position: relative;
+
+        .swiper_slide {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 394px;
+            padding-left: 20px;
+            padding-right: 20px;
+        }
+    }
+
     .swiper-horizontal {
         padding-bottom: 20px;
     }
 
-    .swiper-button-next , .swiper-button-prev {
+    .button_prev {
+        width: 50px;
+        height: 45px;
+        position: absolute;
+        top: 50%;
+        left: 0;
+        transform: translateY(-50%);
+        z-index: 10;
         color: #00856F;
+        background-color: #DCE9E2;
+        border: none;
+        border-radius: 10px;
+        cursor: pointer;
+       
+
+        i {
+            font-size: 24px;
+        }
+    }
+    .button_prev.desable {
+        display: none;
+    }
+    
+    .button_next {
+        width: 50px;
+        height: 45px;
+        position: absolute;
+        top: 50%;
+        right: 0;
+        transform: translateY(-50%);
+        z-index: 10;
+        color: #00856F;
+        background-color: #DCE9E2;
+        border: none;
+        border-radius: 10px;
+        cursor: pointer;
+        
+
+        i {
+            font-size: 24px;
+        }
+    }
+
+    .button_next.desable {
+        display: none;
+    }
+`
+
+export const PlusEvents = styled.div`
+    width: 100px;
+    height: 70px;
+    line-height: 70px;
+    font-size: 16px;
+    text-align: center;
+    color: white;
+    background-color: #00856F;
+
+    border-radius: 10px;
+
+    cursor: pointer;
+
+    &&:hover {
+        background-color: #03C5A5;
     }
 `
