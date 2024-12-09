fd1b157527c94fded97d374c307520c820c0cf27
Jose Wendel <wendelpviana@gmail.com>
2024-12-09 11:42:06 -0300

    :bug: fix: Fixed event single page invalid date bug

--- src/utils/functionsFormatDateHour.ts
+++ src/utils/functionsFormatDateHour.ts
@@ -1,13 +1,15 @@
 import { format } from "date-fns"
 
 export function formatDate(date: string) {
-    const newFormatdate = format(new Date(date), "dd/MM/yyyy")
-    
-    return newFormatdate
+    if(date) {
+        const newFormatdate = format(new Date(date), "dd/MM/yyyy")
+        return newFormatdate
+    }
 }
 
 export function formatHour(hour: string) {
-    const newFormatHour = format(new Date(hour), "HH:mm")
-    
-    return newFormatHour
+    if(hour) {
+        const newFormatHour = format(new Date(hour), "HH:mm")
+        return newFormatHour
+    }
 }
